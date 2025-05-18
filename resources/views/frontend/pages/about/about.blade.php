2e030e2c5e0c13ddf821c24962c07e460b2cb40c
Szamansaju <szamansaju@gmail.com>
2025-05-18 16:16:21 +0600

    some changes done

--- resources/views/frontend/pages/about/about.blade.php
+++ resources/views/frontend/pages/about/about.blade.php
@@ -35,20 +35,16 @@
         }
     </style>
     <section class="ban_sec section_one">
-        <div class="container-fluid p-0">
+        <div class="p-0 container-fluid">
             <div class="ban_img">
-                <img src="https://templates.thememodern.com/industris/images/subheader-about.jpg" alt="banner"
-                    border="0">
-                <div class="ban_text">
-                    <strong>
-                        About Us
-                    </strong>
-                    <ul class="d-flex align-items-center ps-0 mt-5">
+                <img src="{{ asset('img/About-Us-Page-Image_Tech-Focus.png') }}" alt="banner">
+                {{-- <div class="ban_text">
+                    <ul class="mt-5 d-flex align-items-center ps-0">
                         <li class="text-white"><a href="#" class="">Home</a></li>
                         <li class="text-white"><span class="me-2 ms-2">/</span></li>
                         <li class="main-color"><a href="#">About Us</a></li>
                     </ul>
-                </div>
+                </div> --}}
             </div>
         </div>
     </section>
@@ -58,11 +54,12 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div>
-                        <span class="main-color">{{ $aboutPage->section_two_badge }}</span>
+                        <h6 class="main-color">{{ $aboutPage->section_two_badge }}</h6>
                         <div class="mt-lg-2">
-                            <h1 class="title fw-bold">{{ $aboutPage->section_two_title_1 }} <span
-                                    class="main-color">{{ $aboutPage->section_two_title_span }} </span></h1>
-                            <h3 class="py-lg-3">{{ $aboutPage->section_two_subtitle }} </h3>
+                            <h1 class=" fw-bold">{{ $aboutPage->section_two_title_1 }}
+                                {{ $aboutPage->section_two_title_span }}
+                            </h1>
+                            <h3 class="">{{ $aboutPage->section_two_subtitle }} </h3>
                             <p class="">{{ $aboutPage->section_two_description }} </p>
                         </div>
                         {{-- <div class="my-lg-5">
@@ -78,20 +75,22 @@
                 <div class="col-lg-6">
                     <div class="d-flex justify-content-center">
                         <img class="img-fluid" width="470" height="541"
-                            src="{{ !empty($aboutPage->section_two_main_image) && file_exists(public_path('app/public/about-us/' . $aboutPage->section_two_main_image)) ? asset('app/public/about-us/' . $aboutPage->section_two_main_image) : asset('backend/images/no-image-available.png') }}"
-                            alt="">
+                            src="{{ !empty($aboutPage->section_two_main_image) && file_exists(public_path('app/public/about-us/' . $aboutPage->section_two_main_image)) ? asset('app/public/about-us/' . $aboutPage->section_two_main_image) : 'https://img.freepik.com/free-photo/about-as-service-contact-information-concept_53876-138509.jpg?semt=ais_hybrid&w=740' }}"
+                            alt="Main Image"
+                            onerror="this.onerror=null;this.src='https://img.freepik.com/free-photo/about-as-service-contact-information-concept_53876-138509.jpg?semt=ais_hybrid&w=740';" />
                     </div>
                     <div class="showcase">
-                        <img src="{{ !empty($aboutPage->section_two_secondary_image) && file_exists(public_path('app/public/about-us/' . $aboutPage->section_two_secondary_image)) ? asset('app/public/about-us/' . $aboutPage->section_two_secondary_image) : asset('backend/images/no-image-available.png') }}"
-                            alt="Picture">
+                        <img src="{{ !empty($aboutPage->section_two_secondary_image) && file_exists(public_path('app/public/about-us/' . $aboutPage->section_two_secondary_image)) ? asset('app/public/about-us/' . $aboutPage->section_two_secondary_image) : 'https://img.freepik.com/free-photo/about-as-service-contact-information-concept_53876-138509.jpg?semt=ais_hybrid&w=740' }}"
+                            alt="Secondary Image"
+                            onerror="this.onerror=null;this.src='https://img.freepik.com/free-photo/about-as-service-contact-information-concept_53876-138509.jpg?semt=ais_hybrid&w=740';" />
                         <div class="overlay">
                             <h2 class="mb-1">{{ $aboutPage->section_two_secondary_image_count }}</h2>
-                            <p>
-                                {{ $aboutPage->section_two_secondary_image_title }}
-                            </p>
+                            <p>{{ $aboutPage->section_two_secondary_image_title }}</p>
+
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </section>
@@ -99,48 +98,49 @@
     <section class="section_three">
         <div class="container">
             <ul class="nav nav-tabs row g-2" id="myTab" role="tablist">
-                <li class="nav-item col-lg-3 p-3 ps-0" role="presentation">
-                    <button class="nav-link nav-links shadow-sm p-4 active" id="home-tab" data-bs-toggle="tab"
-                        data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">
+                <li class="p-3 nav-item col-lg-3 ps-0" role="presentation">
+                    <button class="p-4 shadow-sm nav-link nav-links active" id="home-tab" data-bs-toggle="tab"
+                        style="height: 200px" data-bs-target="#home" type="button" role="tab" aria-controls="home"
+                        aria-selected="true">
                         <div class=" text-start">
                             <div>
-                                <h3>{{ $aboutPage->section_three_tab_one_title }}</h3>
+                                <h6>{{ $aboutPage->section_three_tab_one_title }}</h6>
                                 <p>{{ $aboutPage->section_three_tab_one_short_description }}</p>
                             </div>
                         </div>
                     </button>
                 </li>
-                <li class="nav-item col-lg-3 p-3" role="presentation">
-                    <button class="nav-link nav-links shadow-sm p-4" id="profile-tab" data-bs-toggle="tab"
-                        data-bs-target="#profile" type="button" role="tab" aria-controls="profile"
-                        aria-selected="false">
+                <li class="p-3 nav-item col-lg-3" role="presentation">
+                    <button class="p-4 shadow-sm nav-link nav-links" id="profile-tab" data-bs-toggle="tab"
+                        style="height: 200px" data-bs-target="#profile" type="button" role="tab"
+                        aria-controls="profile" aria-selected="false">
                         <div class="text-start">
                             <div>
-                                <h3>{{ $aboutPage->section_three_tab_two_title }}</h3>
+                                <h6>{{ $aboutPage->section_three_tab_two_title }}</h6>
                                 <p>{{ $aboutPage->section_three_tab_two_short_description }}</p>
                             </div>
                         </div>
                     </button>
                 </li>
-                <li class="nav-item col-lg-3 p-3" role="presentation">
-                    <button class="nav-link nav-links shadow-sm p-4" id="contact-tab" data-bs-toggle="tab"
-                        data-bs-target="#contact" type="button" role="tab" aria-controls="contact"
-                        aria-selected="false">
+                <li class="p-3 nav-item col-lg-3" role="presentation">
+                    <button class="p-4 shadow-sm nav-link nav-links" id="contact-tab" data-bs-toggle="tab"
+                        style="height: 200px" data-bs-target="#contact" type="button" role="tab"
+                        aria-controls="contact" aria-selected="false">
                         <div class="text-start">
                             <div>
-                                <h3>{{ $aboutPage->section_three_tab_three_title }}</h3>
+                                <h6>{{ $aboutPage->section_three_tab_three_title }}</h6>
                                 <p>{{ $aboutPage->section_three_tab_three_short_description }}</p>
                             </div>
                         </div>
                     </button>
                 </li>
-                <li class="nav-item col-lg-3 p-3 pe-0" role="presentation">
-                    <button class="nav-link nav-links shadow-sm p-4" id="contact-tabs" data-bs-toggle="tab"
-                        data-bs-target="#contacts" type="button" role="tab" aria-controls="contacts"
-                        aria-selected="false">
+                <li class="p-3 nav-item col-lg-3 pe-0" role="presentation">
+                    <button class="p-4 shadow-sm nav-link nav-links" id="contact-tabs" data-bs-toggle="tab"
+                        style="height: 200px" data-bs-target="#contacts" type="button" role="tab"
+                        aria-controls="contacts" aria-selected="false">
                         <div class="text-start">
                             <div>
-                                <h3>{{ $aboutPage->section_three_tab_four_title }}</h3>
+                                <h6>{{ $aboutPage->section_three_tab_four_title }}</h6>
                                 <p>{{ $aboutPage->section_three_tab_four_short_description }}</p>
                             </div>
                         </div>
@@ -149,7 +149,7 @@
             </ul>
             <div class="tab-content custom-spacer" id="myTabContent">
                 <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                    <div class="row align-items-center bg-white p-5">
+                    <div class="p-5 bg-white row align-items-center">
                         <div class="col-lg-7">
                             <h2>{{ $aboutPage->section_three_tab_one_title }}</h2>
                             <p>{{ $aboutPage->section_three_tab_one_detailed_description }}</p>
@@ -201,7 +201,7 @@
                     </div>
                 </div>
                 <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                    <div class="row align-items-center bg-white p-5">
+                    <div class="p-5 bg-white row align-items-center">
                         <div class="col-lg-7">
                             <h2>{{ $aboutPage->section_three_tab_two_title }}</h2>
                             <p>{{ $aboutPage->section_three_tab_two_detailed_description }}</p>
@@ -253,7 +253,7 @@
                     </div>
                 </div>
                 <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-                    <div class="row align-items-center bg-white p-5">
+                    <div class="p-5 bg-white row align-items-center">
                         <div class="col-lg-7">
                             <h2>{{ $aboutPage->section_three_tab_three_title }}</h2>
                             <p>{{ $aboutPage->section_three_tab_three_detailed_description }}</p>
@@ -305,7 +305,7 @@
                     </div>
                 </div>
                 <div class="tab-pane fade" id="contacts" role="tabpanel" aria-labelledby="contact-tabs">
-                    <div class="row align-items-center bg-white p-5">
+                    <div class="p-5 bg-white row align-items-center">
                         <div class="col-lg-7">
                             <h2>{{ $aboutPage->section_three_tab_four_title }}</h2>
                             <p>{{ $aboutPage->section_three_tab_four_detailed_description }}</p>
@@ -361,10 +361,11 @@
     </section>
 
     <section class="ban_sec-action section_four">
-        <div class="container p-0">
-            <div class="ban_img-action">
-                <img src="{{ !empty($aboutPage->section_four_banner_middle_image) && file_exists(public_path('app/public/about-us/' . $aboutPage->section_four_banner_middle_image)) ? asset('app/public/about-us/' . $aboutPage->section_four_banner_middle_image) : asset('backend/images/no-image-available.png') }}"
-                    alt="banner" border="0">
+        <div class="p-0 ">
+            <div class="">
+                <img class="img-fluid w-100"
+                    src="{{ !empty($aboutPage->section_four_banner_middle_image) && file_exists(public_path('app/public/about-us/' . $aboutPage->section_four_banner_middle_image)) ? asset('app/public/about-us/' . $aboutPage->section_four_banner_middle_image) : asset('img/about-us-banner.jpg') }}"
+                    alt="banner">
                 <div class="ban_text-action">
                 </div>
             </div>
@@ -377,13 +378,15 @@
                 <!-- Column 1 -->
                 <div class="col-lg-6" style="border-right: 1px solid var(--secondary-color)">
                     <div class="me-4">
-                        <h1 class="pb-5">{{ $aboutPage->section_five_col_one_title }}</h1>
+                        <h1>{{ $aboutPage->section_five_col_one_title }}</h1>
                         <p class="pb-5">{{ $aboutPage->section_five_col_one_description }}</p>
-                        <div>
+                        <div class="d-flex justify-content-start align-items-start">
                             @if ($aboutPage->section_five_ceo_sign)
-                                <img class="img-fluid" src="{{ $aboutPage->section_five_ceo_sign }}"
-                                    alt="CEO Signature">
+                                <img class="img-fluid" width="200px" src="{{ $aboutPage->section_five_ceo_sign }}"
+                                    alt="CEO Signature"
+                                    onerror="this.onerror=null;this.src='{{ asset('img/ceo.png') }}';">
                             @endif
+
                         </div>
                         <div class="d-flex">
                             <div class="p-2 pe-3" style="border-right: 1px solid black">
@@ -393,15 +396,15 @@
                             <div class="d-flex align-items-center">
                                 @if ($aboutPage->section_five_ceo_facebook_account_link)
                                     <a href="{{ $aboutPage->section_five_ceo_facebook_account_link }}"><i
-                                            class="fa-brands fa-square-facebook p-2"></i></a>
+                                            class="p-2 fa-brands fa-square-facebook"></i></a>
                                 @endif
                                 @if ($aboutPage->section_five_ceo_twitter_account_link)
                                     <a href="{{ $aboutPage->section_five_ceo_twitter_account_link }}"><i
-                                            class="fa-brands fa-twitter p-2"></i></a>
+                                            class="p-2 fa-brands fa-twitter"></i></a>
                                 @endif
                                 @if ($aboutPage->section_five_ceo_whatsapp_account_link)
                                     <a href="{{ $aboutPage->section_five_ceo_whatsapp_account_link }}"><i
-                                            class="fa-brands fa-whatsapp p-2"></i></a>
+                                            class="p-2 fa-brands fa-whatsapp"></i></a>
                                 @endif
                             </div>
                         </div>
@@ -442,11 +445,12 @@
     </section>
 
     <section class="section_six">
-        <div class="container px-4 custom-spacer mt-5">
+        <div class="container px-4 mt-5 custom-spacer">
             <div class="row gx-5">
                 <!-- Card 1 -->
                 <div class="col-lg-3 ps-0">
-                    <div class="px-4 py-5 shadow-lg text-center" style="border-bottom: 1px solid var(--primary-color)">
+                    <div class="px-4 py-5 text-center shadow-lg"
+                        style="border-bottom: 1px solid var(--primary-color); height: 350px;">
                         @if ($aboutPage->section_six_card_one_icon)
                             <i class="{{ $aboutPage->section_six_card_one_icon }} pe-2 main-color"></i>
                         @endif
@@ -456,7 +460,8 @@
                 </div>
                 <!-- Card 2 -->
                 <div class="col-lg-3 ps-0">
-                    <div class="px-4 py-5 shadow-lg text-center" style="border-bottom: 1px solid var(--primary-color)">
+                    <div class="px-4 py-5 text-center shadow-lg"
+                        style="border-bottom: 1px solid var(--primary-color); height: 350px;">
                         @if ($aboutPage->section_six_card_two_icon)
                             <i class="{{ $aboutPage->section_six_card_two_icon }} pe-2 main-color"></i>
                         @endif
@@ -466,7 +471,8 @@
                 </div>
                 <!-- Card 3 -->
                 <div class="col-lg-3 ps-0">
-                    <div class="px-4 py-5 shadow-lg text-center" style="border-bottom: 1px solid var(--primary-color)">
+                    <div class="px-4 py-5 text-center shadow-lg"
+                        style="border-bottom: 1px solid var(--primary-color); height: 350px;">
                         @if ($aboutPage->section_six_card_three_icon)
                             <i class="{{ $aboutPage->section_six_card_three_icon }} pe-2 main-color"></i>
                         @endif
@@ -477,7 +483,8 @@
                 </div>
                 <!-- Card 4 -->
                 <div class="col-lg-3 ps-0 pe-0">
-                    <div class="px-4 py-5 shadow-lg text-center" style="border-bottom: 1px solid var(--primary-color)">
+                    <div class="px-4 py-5 text-center shadow-lg"
+                        style="border-bottom: 1px solid var(--primary-color); height: 350px;">
                         @if ($aboutPage->section_six_card_four_icon)
                             <i class="{{ $aboutPage->section_six_card_four_icon }} pe-2 main-color"></i>
                         @endif
@@ -490,12 +497,14 @@
         </div>
     </section>
     <section class="section_seven">
-        <div class="container p-0 custom-spacer pt-5">
+        <div class="container p-0 pt-5 custom-spacer">
             <h2 class="text-center">Our Brands</h2>
             <div class="customer-logos slider">
                 @foreach ($brands as $brand)
                     <div class="slide">
-                        <img src="{{ !empty($brand->logo) && file_exists(public_path('storage/brand/logo/' . $brand->logo)) ? asset('storage/brand/logo/' . $brand->logo) : asset('backend/images/no-image-available.png') }}" alt="">
+                        <img class="img-fluid" style="width: 150px; height: 150px; object-fit: contain;"
+                            src="{{ !empty($brand->logo) && file_exists(public_path('storage/brand/logo/' . $brand->logo)) ? asset('storage/brand/logo/' . $brand->logo) : asset('backend/images/no-image-available.png') }}"
+                            alt="">
                     </div>
                 @endforeach
             </div>
