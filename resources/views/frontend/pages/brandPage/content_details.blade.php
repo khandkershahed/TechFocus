16a35cb840fdeca05fd2a0eb318f958d8d93179f
shahed141123 <khandkershahed23@gmail.com>
2025-10-26 14:07:56 +0600

    Page Banner

--- resources/views/frontend/pages/brandPage/content_details.blade.php
+++ resources/views/frontend/pages/brandPage/content_details.blade.php
@@ -4,29 +4,24 @@
 @endsection
 
 @section('content')
-
     {{-- Brand Page Header --}}
-    @if(isset($brand) && $brand)
-        @include('frontend.pages.brandPage.partials.page_header', ['brand' => $brand])
-    @endif
+    {{-- <section>
+        <div class="brand-page-banner page_top_banner">
+            <img src="{{ !empty(optional($brand->brandPage)->banner_image) && file_exists(public_path('storage/brand-page/banner-image/' . optional($brand->brandPage)->banner_image)) ? asset('storage/brand-page/banner-image/' . optional($brand->brandPage)->banner_image) : asset('frontend/images/no-banner(1920-330).png') }}"
+                alt="">
+        </div>
+    </section> --}}
 
     {{-- Banner Section --}}
-    @if(isset($newsTrend) && $newsTrend)
-        <section>
-            <div class="brand-page-banner page_top_banner">
-                <img src="{{ $newsTrend->banner_image && file_exists(public_path('storage/' . $newsTrend->banner_image))
-                    ? asset('storage/' . $newsTrend->banner_image)
-                    : asset('frontend/images/no-banner(1920-330).png') }}"
-                    alt="{{ $newsTrend->title ?? 'Banner' }}">
-            </div>
-        </section>
-    @else
-        <section>
-            <div class="brand-page-banner page_top_banner">
-                <img src="{{ asset('frontend/images/no-banner(1920-330).png') }}" alt="No banner available">
-            </div>
-        </section>
-    @endif
+
+    <section>
+        <div class="brand-page-banner page_top_banner">
+            <img src="{{ !empty($newsTrend->banner_image) && file_exists(public_path('storage/content/' . $newsTrend->banner_image))
+                ? asset('storage/content/' . $newsTrend->banner_image)
+                : asset('frontend/images/no-banner(1920-330).png') }}"
+                alt="{{ $newsTrend->title ?? 'Banner' }}">
+        </div>
+    </section>
 
     {{-- Main Content --}}
     <div class="container mt-5">
@@ -37,25 +32,25 @@
                 <div class="card border-0 shadow-sm">
                     <div class="card-body p-lg-5 p-4">
 
-                        @if(isset($newsTrend) && $newsTrend->badge)
+                        @if (isset($newsTrend) && $newsTrend->badge)
                             <h2 class="subtitles fw-bold">{{ $newsTrend->badge }}</h2>
                         @endif
 
-                        @if(isset($newsTrend) && $newsTrend->title)
+                        @if (isset($newsTrend) && $newsTrend->title)
                             <h3 class="titles main-color py-2">{{ $newsTrend->title }}</h3>
                         @endif
 
-                        @if(isset($newsTrend) && $newsTrend->header)
+                        @if (isset($newsTrend) && $newsTrend->header)
                             <h5>{{ $newsTrend->header }}</h5>
                         @endif
 
-                        @if(isset($newsTrend) && $newsTrend->short_des)
+                        @if (isset($newsTrend) && $newsTrend->short_des)
                             <h6 style="border-left: 3px solid #001430; padding-left:10px">
                                 {!! $newsTrend->short_des !!}
                             </h6>
                         @endif
 
-                        @if(isset($newsTrend) && $newsTrend->long_des)
+                        @if (isset($newsTrend) && $newsTrend->long_des)
                             <p class="text-justify" style="line-height: 1.5;">
                                 {!! $newsTrend->long_des !!}
                             </p>
@@ -68,8 +63,7 @@
             {{-- Right Column: Brand Info --}}
             <div class="col-lg-3">
                 <div class="card border-0 shadow-sm">
-                    <div class="card-header rounded-0 bg-white"
-                        style="border-top:5px solid #001430; border-bottom: 0;">
+                    <div class="card-header rounded-0 bg-white" style="border-top:5px solid #001430; border-bottom: 0;">
                         <div class="d-flex justify-content-center">
                             <img class="rounded-circle"
                                 src="{{ isset($brand) && $brand->logo && file_exists(public_path('storage/' . $brand->logo))
@@ -79,15 +73,16 @@
                         </div>
 
                         <div class="text-center mt-4">
-                            @if(isset($brand) && $brand)
+                            @if (isset($brand) && $brand)
                                 <h3 class="fw-bolder">{{ $brand->title }}</h3>
                                 <h6>
                                     <i class="fa-solid fa-user main-color"></i> {{ $brand->title }}
                                 </h6>
                             @endif
 
-                            @if(isset($newsTrend) && $newsTrend->address)
-                                <a href="{{ $newsTrend->source_link ?? '#' }}" class="border p-2 rounded-pill shadow-2 mt-2 d-inline-block">
+                            @if (isset($newsTrend) && $newsTrend->address)
+                                <a href="{{ $newsTrend->source_link ?? '#' }}"
+                                    class="border p-2 rounded-pill shadow-2 mt-2 d-inline-block">
                                     <i class="fa-solid fa-location-dot main-color"></i>
                                     {{ $newsTrend->address }}
                                 </a>
@@ -99,7 +94,7 @@
         </div>
 
         {{-- Footer Note --}}
-        @if(isset($newsTrend) && $newsTrend->footer)
+        @if (isset($newsTrend) && $newsTrend->footer)
             <div class="row mx-lg-5 mx-3 mt-2 mb-5">
                 <div class="col-12" style="border-top: 3px dashed #001430; border-bottom: 3px dashed #001430;">
                     <p class="sub-color fw-semibold text-center mb-0 py-3">
@@ -110,5 +105,4 @@
         @endif
 
     </div>
-
 @endsection
