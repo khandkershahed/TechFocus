b8449681b085570535133b8c7cb5625d75c9049d
Szamansaju <szamansaju@gmail.com>
2025-11-06 11:03:20 +0600

    Category Page updated

--- resources/views/frontend/pages/shop/filterProducts.blade.php
+++ resources/views/frontend/pages/shop/filterProducts.blade.php
@@ -47,68 +47,94 @@
             </div>
         </div>
     </div>
-
-    <!-- Filter Form -->
-    <form id="filterForm" method="GET" action="{{ route('filtering.products', $category->slug) }}">
-        <div class="mx-3 mt-3 row">
-            <!-- Sidebar Filters -->
-            <div class="col-lg-3 col-sm-12">
-
-                <!-- What's New -->
-                <div class="p-3 mt-3 mb-2 bg-white category-border-top rounded-3">
-                    <div class="checkbox-wrapper-21">
-                        <label class="control control--checkbox">
-                            What's New
-                            <input type="checkbox" name="whats_new" value="1" {{ request('whats_new') == '1' ? 'checked' : '' }}>
-                            <div class="control__indicator"></div>
-                        </label>
-                    </div>
+    <!-- Section Container -->
+    <div class="mx-3 mt-3 row">
+        <div class="col-lg-3 col-sm-12">
+            <div class="p-3 mt-3 mb-2 bg-white category-border-top rounded-3">
+                <div class="checkbox-wrapper-21">
+                    <label class="control control--checkbox">
+                        What's New
+                        <input type="checkbox" name="whats_new" value="1"
+                            onchange="document.getElementById('filterForm').submit()"
+                            {{ request('whats_new') == '1' ? 'checked' : '' }} />
+                        <div class="control__indicator"></div>
+                    </label>
                 </div>
+            </div>
+            <!-- Main Category Start-->
+            <!-- Manufacturers -->
+            <div class="p-3 my-3 bg-white category-border-top rounded-3">
+                <div class="accordion accordion-flush" id="accordionFlushExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingOne">
+                            <button class="p-1 border-0 accordion-button collapsed" type="button"
+                                data-bs-toggle="collapse" data-bs-target="#flush-collapseOnemanufactureer"
+                                aria-expanded="false" aria-controls="flush-collapseOnemanufactureer">
+                                Manufacturers
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOnemanufactureer" class="border-0 accordion-collapse collapse"
+                            aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                            <div class="p-0 m-0 accordion-body">
+                                <div class="pt-3 position-relative">
+                                    <input
+                                        id="autocomplete_company"
+                                        type="text"
+                                        class="py-2 form-control ps-5 rounded-3"
+                                        placeholder="Search...">
+                                    <i class="pt-3 fa fa-search position-absolute top-50 start-0 translate-middle-y ms-3 text-secondary"></i>
+                                </div>
+                                <!-- Menu -->
+                                <div class="mt-3 scroll-menu-container" style="height: 500px; overflow: auto">
+                                    <ul class="p-0 m-0" id="brandList">
+                                        @foreach ($brands as $brand)
+                                        <li class="p-2 brand-item">
 
-                <!-- Brands -->
-                <div class="p-3 my-3 bg-white category-border-top rounded-3">
-                    <div class="accordion accordion-flush" id="accordionFlushExample">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="flush-headingOne">
-                                <button class="p-1 border-0 accordion-button collapsed" type="button"
-                                    data-bs-toggle="collapse" data-bs-target="#flush-collapseOnemanufactureer"
-                                    aria-expanded="false" aria-controls="flush-collapseOnemanufactureer">
-                                    Manufacturers
-                                </button>
-                            </h2>
-                            <div id="flush-collapseOnemanufactureer" class="border-0 accordion-collapse collapse"
-                                 aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                <div class="p-0 m-0 accordion-body">
-                                    <!-- Brand search -->
-                                    <div class="pt-3 position-relative">
-                                        <input id="autocomplete_company" type="text"
-                                               class="py-2 form-control ps-5 rounded-3" placeholder="Search...">
-                                        <i class="pt-3 fa fa-search position-absolute top-50 start-0 translate-middle-y ms-3 text-secondary"></i>
-                                    </div>
+                                            <div class="checkbox-wrapper-21">
+                                                <label class="control control--checkbox">
+                                                    {{ $brand->title }}
+                                                    <input type="checkbox" name="brand_id" value="{{ $brand->id }}"
+                                                        onchange="document.getElementById('filterForm').submit()"
+                                                        {{ request('brand_id') == $brand->id ? 'checked' : '' }} />
+                                                    <div class="control__indicator"></div>
+                                                </label>
+                                            </div>
 
-                                    <!-- Brand List -->
-                                    <div class="mt-3 scroll-menu-container" style="height: 500px; overflow: auto">
-                                        <ul class="p-0 m-0" id="brandList">
-                                            @foreach ($brands as $brand)
-                                                <li class="p-2 brand-item">
-                                                    <div class="checkbox-wrapper-21">
-                                                        <label class="control control--checkbox">
-                                                            {{ $brand->title }}
-                                                            <input type="checkbox" name="brand_id[]" value="{{ $brand->id }}"
-                                                                {{ in_array($brand->id, (array) request('brand_id')) ? 'checked' : '' }}>
-                                                            <div class="control__indicator"></div>
-                                                        </label>
-                                                    </div>
-                                                </li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
+                                        </li>
+                                        @endforeach
+                                    </ul>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
+            </div>
+        </div>
+        <!-- Main Category End-->
+        <div class="col-lg-9 col-sm-12">
+            <div class="p-1 mt-2 border-0 card rounded-0">
+                <div class="bg-white border-0 card-header d-flex align-items-center">
+                    <div>
+                        <img src="https://img.virtual-expo.com/media/ps/images/di/logos/GA-icon.svg" width="25px"
+                            height="25px" alt="" />
+                    </div>
+                    <h6 class="mb-0 ms-3 fw-bold">
+                        {{ $category->name }} | Choosing the right {{ $category->name }}
+                    </h6>
+                </div>
+                <div class="card-body">
+                    <p style="line-height: 1.1">
+                        Measuring pressure is necessary in the control of most
+                        industrial processes. A pressure sensor converts pressure
+                        information into an electrical signal. Most pressure sensors
+                        measure the deformation of a membrane under the effect of the
+                        difference in the pressure applied to both sides.
+                        Manufacturers use different terms for these products. While
+                        “pressure sensor” and “pressure transducer” can be considered
+                        synonyms, the term “pressure transmitter” refers to a...
+                    </p>
+                    <a href="" class="w-auto mt-2 btn signin rounded-0">Read More</a>
+                </div>
             </div>
             <div class="mt-3 row gx-3">
                 @foreach ($products as $product)
@@ -120,7 +146,7 @@
                                 src="{{ $product->thumbnail }}"
                                 alt="{{ $product->name }}"
                                 class="w-100"
-                                style="height: 100%; object-fit: cover;">
+                                style="height: 250px; object-fit: contain;">
                             <!-- Brand Logo (Overlay Top Right) -->
                             <a href="{{ route('brand.overview', optional($product->brand)->slug) }}"
                                 class="top-0 p-1 m-2 bg-white rounded shadow-sm position-absolute end-0">
