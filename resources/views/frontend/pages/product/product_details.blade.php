7404efaac4466a6c2e6c19b3c19d918a7a474994
Szamansaju <szamansaju@gmail.com>
2025-12-07 10:38:31 +0600

    Pull done

--- resources/views/frontend/pages/product/product_details.blade.php
+++ resources/views/frontend/pages/product/product_details.blade.php
@@ -182,7 +182,7 @@
         </div>
     </div>
 
-    <<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
@@ -201,9 +201,6 @@
         });
 
        
-        
-   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
 $(document).ready(function() {
